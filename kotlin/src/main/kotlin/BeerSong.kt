f55f42512f37373c64125e73783462a8c95151d4
Daniel Teske <danielteske@dante.local>
2017-12-19 16:56:23 +0100

    allVerses refactored

--- kotlin/src/main/kotlin/BeerSong.kt
+++ kotlin/src/main/kotlin/BeerSong.kt
@@ -3,11 +3,11 @@ class BeerSong {
     companion object {
 
         fun verse(bottleCount: Int): String {
-            when (bottleCount) {
-                0 -> return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-                1 -> return "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-                2 -> return "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-                else -> return "$bottleCount bottles of beer on the wall, $bottleCount bottles of beer.\nTake one down and pass it around, ${bottleCount-1} bottles of beer on the wall.\n"
+            return when (bottleCount) {
+                0 -> "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+                1 -> "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+                2 -> "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+                else -> "$bottleCount bottles of beer on the wall, $bottleCount bottles of beer.\nTake one down and pass it around, ${bottleCount-1} bottles of beer on the wall.\n"
             }
         }
 
