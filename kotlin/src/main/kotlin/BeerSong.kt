168af2aca2ffe1c1cf8c14c44cde21caf03867d6
Daniel Teske <danielteske@dante.local>
2017-12-19 16:49:57 +0100

    firstTwoVerses green

--- kotlin/src/main/kotlin/BeerSong.kt
+++ kotlin/src/main/kotlin/BeerSong.kt
@@ -4,10 +4,18 @@ class BeerSong {
 
         fun verse(bottleCount: Int): String {
             when (bottleCount) {
+                0 -> return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
                 1 -> return "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
                 2 -> return "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
                 else -> return "$bottleCount bottles of beer on the wall, $bottleCount bottles of beer.\nTake one down and pass it around, ${bottleCount-1} bottles of beer on the wall.\n"
             }
+        }
+
+        fun verses(to: Int, from: Int): String {
+            return (from..to)
+                    .reversed()
+                    .map({ verse(it) })
+                    .joinToString("\n")
         }
 
     }
--- kotlin/src/test/kotlin/BeerSongTest.kt
+++ kotlin/src/test/kotlin/BeerSongTest.kt
@@ -29,21 +29,18 @@ class BeerSongTest {
         assertEquals(expected, BeerSong.verse(1))
     }
 
-    @Ignore
     @Test
     fun verse0() {
         val expected = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-        //assertEquals(expected, BeerSong.verse(0))
+        assertEquals(expected, BeerSong.verse(0))
     }
 
-    @Ignore
     @Test
     fun firstTwoVerses() {
         val expected = "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n\n98 bottles of beer on the wall, 98 bottles of beer.\nTake one down and pass it around, 97 bottles of beer on the wall.\n"
-        //assertEquals(expected, BeerSong.verses(99, 98))
+        assertEquals(expected, BeerSong.verses(99, 98))
     }
-
-    @Ignore
+    
     @Test
     fun lastThreeVerses() {
         val expected = "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n\n1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
