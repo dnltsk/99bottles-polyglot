55809be1a1ba1e4dbc5e5502fab869d33e490176
Daniel Teske <danielteske@dante.local>
2017-12-19 16:13:46 +0100

    verse2 refactored

--- kotlin/src/main/kotlin/BeerSong.kt
+++ kotlin/src/main/kotlin/BeerSong.kt
@@ -6,7 +6,8 @@ class BeerSong {
             return "$bottleCount bottles of beer on the wall, " +
                     "$bottleCount bottles of beer.\n" +
                     "Take one down and pass it around, " +
-                    "${bottleCount-1} bottle${if (bottleCount>2) "s" else ""} of beer on the wall.\n"
+                    "${bottleCount-1} bottle${if (bottleCount>2) "s" else ""} " +
+                    "of beer on the wall.\n"
         }
 
     }
