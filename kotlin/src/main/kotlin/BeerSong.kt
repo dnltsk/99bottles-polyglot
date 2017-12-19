04b1dddab38d55a3292861761d7e59dbe35287aa
Daniel Teske <danielteske@dante.local>
2017-12-19 16:07:41 +0100

    lastGenericVerse refactored

--- kotlin/src/main/kotlin/BeerSong.kt
+++ kotlin/src/main/kotlin/BeerSong.kt
@@ -3,7 +3,10 @@ class BeerSong {
     companion object {
 
         fun verse(bottleCount: Int): String {
-            return "$bottleCount bottles of beer on the wall, $bottleCount bottles of beer.\nTake one down and pass it around, ${bottleCount-1} bottles of beer on the wall.\n"
+            return "$bottleCount bottles of beer on the wall, " +
+                    "$bottleCount bottles of beer.\n" +
+                    "Take one down and pass it around, " +
+                    "${bottleCount-1} bottles of beer on the wall.\n"
         }
 
     }
