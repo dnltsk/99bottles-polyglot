cf59d65c5af11178667688fcee88551258f7d82c
Daniel Teske <danielteske@dante.local>
2017-12-19 16:18:34 +0100

    verse1 refactored

--- kotlin/src/main/kotlin/BeerSong.kt
+++ kotlin/src/main/kotlin/BeerSong.kt
@@ -6,10 +6,8 @@ class BeerSong {
             when (bottleCount) {
                 1 -> return "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
                 2 -> return "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-                3 -> return "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n"
-                99 -> return "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n"
+                else -> return "$bottleCount bottles of beer on the wall, $bottleCount bottles of beer.\nTake one down and pass it around, ${bottleCount-1} bottles of beer on the wall.\n"
             }
-            throw NotImplementedError()
         }
 
     }
